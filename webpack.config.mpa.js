8dde53fc78116021c325b02bfc15c6c970fb802c
dajun1230 <1123395547@qq.com>
2021-06-15 07:46:31 +0800

    feat: Multi portal configuration

--- webpack.config.mpa.js
+++ webpack.config.mpa.js
@@ -2,29 +2,55 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const glob = require('glob');
+
+const setMpa = () => {
+  const entry = {};
+  const htmlWebpackPlugins = [];
+
+  // 生成入口
+  const entryFiles = glob.sync(path.join(__dirname, "./src/*/index.js"))
+
+  entryFiles.map((item, index) => {
+    const entryFile = item;
+    const match = entryFile.match(/src\/(.*)\/index\.js$/);
+    const pageName = match[1];
+    entry[pageName] = entryFile;
+
+    htmlWebpackPlugins.push(new HtmlWebpackPlugin({
+      template: path.join(__dirname, `./src/${pageName}/index.html`),
+      filename: `${pageName}.html`,
+      chunks: [pageName]
+    }))
+  })
+
+  return {
+    entry,
+    htmlWebpackPlugins
+  }
+}
+
+const { entry, htmlWebpackPlugins } = setMpa();
 
 module.exports = {
   mode: 'development',
   devtool: 'source-map',
   // 入口
-  entry: './src/index.js',
+  // entry: './src/index.js',
   // 等价于如下：
   // entry: {
   //   main: './src/index.js'
   // },
-  // entry: {
-  //   main: './src/index.js',
-  //   other: './src/other.js'
-  // },
+  entry: entry,
   // 出口
   // output: './dist',
   output: {
     // 输出资源的存放位置，必须是绝对路径
     path: path.resolve(__dirname, './dist'),
     // 输出后的文件名称，将所有依赖的模块合并输出到main.js，默认为main.js
-    filename: 'main.js',
+    // filename: 'main.js',
     // 占位符
-    // filename: '[name]-[chunkhash:6].js'
+    filename: '[name]-[chunkhash:6].js'
   },
   module: {
     rules: [
@@ -93,16 +119,17 @@ module.exports = {
     modules: ["node_modules", "./src/myLoaders"]
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      filename: 'index.html',
-      chunks: ['main']
-    }),
+    // new HtmlWebpackPlugin({
+    //   template: './public/index.html',
+    //   filename: 'index.html',
+    //   chunks: ['main']
+    // }),
     // new HtmlWebpackPlugin({
     //   template: './public/other.html',
     //   filename: 'other.html',
     //   chunks: ['other']
     // }),
+    ...htmlWebpackPlugins,
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
       filename: 'css/index-[chunkhash:6].css',
